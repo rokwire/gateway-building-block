e5ffe677ed5bc74c386514285c878b6d73cc266c
Stephen Hurwit <sjhurwit@gmail.com>
2022-07-14 09:09:06 -0700

    update license headers

--- driven/storage/adapter.go
+++ driven/storage/adapter.go
@@ -1,30 +1,28 @@
-/*
- *   Copyright (c) 2020 Board of Trustees of the University of Illinois.
- *   All rights reserved.
-
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
-
- *   http://www.apache.org/licenses/LICENSE-2.0
-
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+// Copyright 2022 Board of Trustees of the University of Illinois.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package storage
 
 import (
 	"context"
 	"fmt"
-	"github.com/google/uuid"
-	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"strconv"
 	"time"
+
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Adapter implements the Storage interface
