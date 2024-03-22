846a5d39df02b0607db9499475bdd522b8217dc8
Stefan Vitanov <stefan@inabyte.com>
2024-03-22 13:52:19 +0200

    fix and set the docs for the GET webtools vlacklist API

--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -106,7 +106,8 @@ func (a Adapter) Start() {
 	adminRouter.HandleFunc("/configs/{id}", a.wrapFunc(a.adminAPIsHandler.updateConfig, a.auth.admin.Permissions)).Methods("PUT")
 	adminRouter.HandleFunc("/configs/{id}", a.wrapFunc(a.adminAPIsHandler.deleteConfig, a.auth.admin.Permissions)).Methods("DELETE")
 
-	adminRouter.HandleFunc("/webtoolsblacklist", a.wrapFunc(a.adminAPIsHandler.webtoolsblacklist /*a.auth.admin.Permissions*/, nil)).Methods("POST")
+	adminRouter.HandleFunc("/webtoolsblacklist", a.wrapFunc(a.adminAPIsHandler.webtoolsblacklist, a.auth.admin.Permissions)).Methods("POST")
+	adminRouter.HandleFunc("/webtoolsblacklist", a.wrapFunc(a.adminAPIsHandler.webtoolsblacklist, a.auth.admin.Permissions)).Methods("GET")
 
 	// BB APIs
 	bbsRouter := mainRouter.PathPrefix("/bbs").Subrouter()
