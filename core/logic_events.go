52a992cffbbe14a6a8422ecdc0db7c2221d4e0d9
Petyo Stoyanov <petyo.stoyanov@inabit.eu>
2024-05-05 15:21:07 +0300

    Fix timer

--- core/logic_events.go
+++ core/logic_events.go
@@ -178,7 +178,7 @@ func (e eventsLogic) setupWebToolsTimer() {
 	}
 
 	//wait until it is the correct moment from the day
-	/*location, err := time.LoadLocation("America/Chicago")
+	location, err := time.LoadLocation("America/Chicago")
 	if err != nil {
 		e.logger.Errorf("Error getting location:%s\n", err.Error())
 	}
@@ -198,9 +198,9 @@ func (e eventsLogic) setupWebToolsTimer() {
 		leftToday := 86400 - nowSecondsInDay
 		durationInSeconds = leftToday + desiredMoment // the time which left today + desired moment from tomorrow
 	}
-	log.Println(durationInSeconds) */
-	duration := time.Second * time.Duration(3)
-	//duration := time.Second * time.Duration(durationInSeconds)
+	log.Println(durationInSeconds)
+	//duration := time.Second * time.Duration(3)
+	duration := time.Second * time.Duration(durationInSeconds)
 	e.logger.Infof("setupWebToolsTimer -> first call after %s", duration)
 
 	e.dailyWebToolsTimer = time.NewTimer(duration)
